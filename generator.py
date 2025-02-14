5c37eb2e30d28beff91ccb42a4f9a7b2da258f08
A-Sal3 <asalinas3205@gmail.com>
2025-02-14 16:09:30 -0600

    Programmed first part of Misty

--- generator.py
+++ generator.py
@@ -1,5 +1,10 @@
 from music21 import *
 
+def chrd (n1, n2, n3, n4, dur):
+    result = chord.Chord([n1, n2, n3, n4])
+    result.duration.quarterLength = dur
+    return result
+
 def nte (n, dur):
     result = note.Note(n) if (n != "R") else note.Rest()
     result.duration.quarterLength = dur
@@ -11,14 +16,37 @@ def nteD (n, dur, dot):
     result.duration.dots = dot
     return result
 
-melody = []
+mel = [nte('B-4', 0.5), nte('G4', 0.5), nteD('D4', 2, 1), nte('B-3', 0.5), nte('C4', 0.5), 
+nte('D-4',0.5), nte('C5',0.5), nte('C5',0.5), nte('C5',0.5), nte('C5',0.5), nte('B-4',0.5), nte('G4',0.5), nte('E-4',0.5),
+nte('C4', 2), nte('R', 1/3), nte('G3', 1/3), nte('A-3', 1/3), nte('C4', 1/3), nte('E-4', 1/3), nte('G-4', 1/3),
+nte('B-4', 0.5), nte('B-4', .5), nte('B-4', .5), nte('A-4', .5), nte('B-4', 1), nte('A-4', .5), nte('B-4', .5),
+nte('G4', 1+1/3), nte('A-4', 1/3), nte('B-4', 1/3), nte('E-4', 1+1/3), nte('F4', 1/3), nte('G4', 1/3)]
+
+prog = [nte('R', 1), chrd("E-2", "G2", "B-2", "D3", 4), chrd("F2", "A-2", "B-2", "D-3", 2), 
+chrd("E-2", "G2", "B-2", "D-3", 2), chrd("G2", "A-2", "C3", "E-3", 4), chrd("G-2", "A-2", "B2", "E-3", 2), 
+chrd("F2", "A-2", "B2", "D-3", 2), chrd("E-2", "G2", "B-2", "D3", 2), chrd("E-2", "G2", "B-2", "C3", 2)]
+
+score = stream.Score()
+melody = stream.Part()
+chords = stream.Part()
+
+for elt in mel:
+    melody.append(elt)
+for elt in prog:
+    chords.append(elt)
+
+# part.append([melody,chords])
+score.insert([0, melody])
+score.insert([0, chords])
+
+# print(part)
+score.show()
+
+# play = stream.Stream()
+# for i in melody:
+#     play.append(i)
 
 
-play = stream.Stream()
-for i in melody:
-    play.append(i)
-
-
-play.show('midi')
+# play.show('midi')
 
 # play.show()
