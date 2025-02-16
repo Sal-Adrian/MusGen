abe7e68186ffbc791519d6dfb6a0bb44b92833ae
A-Sal3 <asalinas3205@gmail.com>
2025-02-16 12:02:38 -0600

    Programmed first part of Misty

--- generator.py
+++ generator.py
@@ -16,15 +16,21 @@ def nteD (n, dur, dot):
     result.duration.dots = dot
     return result
 
-mel = [nte('B-4', 0.5), nte('G4', 0.5), nteD('D4', 2, 1), nte('B-3', 0.5), nte('C4', 0.5), 
+mel = [#nte('B-4', 0.5), nte('G4', 0.5), 
+nteD('D4', 2, 1), nte('B-3', 0.5), nte('C4', 0.5), 
 nte('D-4',0.5), nte('C5',0.5), nte('C5',0.5), nte('C5',0.5), nte('C5',0.5), nte('B-4',0.5), nte('G4',0.5), nte('E-4',0.5),
 nte('C4', 2), nte('R', 1/3), nte('G3', 1/3), nte('A-3', 1/3), nte('C4', 1/3), nte('E-4', 1/3), nte('G-4', 1/3),
 nte('B-4', 0.5), nte('B-4', .5), nte('B-4', .5), nte('A-4', .5), nte('B-4', 1), nte('A-4', .5), nte('B-4', .5),
-nte('G4', 1+1/3), nte('A-4', 1/3), nte('B-4', 1/3), nte('E-4', 1+1/3), nte('F4', 1/3), nte('G4', 1/3)]
+nte('G4', 1+1/3), nte('A-4', 1/3), nte('B-4', 1/3), nte('E-4', 1+1/3), nte('F4', 1/3), nte('G4', 1/3),
+nte('A-4', .5), nte('C4', 1), nte('C4', .5), nte('D4', 2/3), nte('E-4', 2/3), nte('F4', 2/3), 
+nte('G4', 7), nte('B-4', .5), nte('G4', .5)]
 
-prog = [nte('R', 1), chrd("E-2", "G2", "B-2", "D3", 4), chrd("F2", "A-2", "B-2", "D-3", 2), 
+prog = [#nte('R', 1), 
+chrd("E-2", "G2", "B-2", "D3", 4), chrd("F2", "A-2", "B-2", "D-3", 2), 
 chrd("E-2", "G2", "B-2", "D-3", 2), chrd("G2", "A-2", "C3", "E-3", 4), chrd("G-2", "A-2", "B2", "E-3", 2), 
-chrd("F2", "A-2", "B2", "D-3", 2), chrd("E-2", "G2", "B-2", "D3", 2), chrd("E-2", "G2", "B-2", "C3", 2)]
+chrd("F2", "A-2", "B2", "D-3", 2), chrd("E-2", "G2", "B-2", "D3", 2), chrd("E-2", "G2", "B-2", "C3", 2),
+chrd("F2", "A-2", "C3", "E-3", 2), chrd("F2", "A-2", "B-2", "D3", 2), chrd("F2", "G2", "B-2", "D3", 2),
+chrd("G2", "B-2", "C3", "E3", 2), chrd("A-2", "C3", "E-3", "F3", 2), chrd("A-2", "B-2", "D3", "F3", 2)]
 
 score = stream.Score()
 melody = stream.Part()
@@ -35,18 +41,7 @@ for elt in mel:
 for elt in prog:
     chords.append(elt)
 
-# part.append([melody,chords])
 score.insert([0, melody])
 score.insert([0, chords])
 
-# print(part)
 score.show()
-
-# play = stream.Stream()
-# for i in melody:
-#     play.append(i)
-
-
-# play.show('midi')
-
-# play.show()
