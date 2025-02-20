eb94103b724c628966a142f64fb26c5fd93328bc
A-Sal3 <asalinas3205@gmail.com>
2025-02-20 16:21:56 -0600

    Chord progression helper function

--- format.py
+++ format.py
@@ -1,9 +1,47 @@
 from music21 import *
 
-def chrd (n1, n2, n3, n4, dur):
-    result = chord.Chord([n1, n2, n3, n4])
-    result.duration.quarterLength = dur
-    return result
+TONES = ["C", "D-", "D", "E-", "E", "F", "G-", "G", "A-", "A", "B-", "B"]
+
+def progression (chords):
+    prog = []
+
+
+    for elt in chords:
+        if elt[0] == "R":
+            prog.append(nte(elt[0], elt[1]))
+            continue
+
+        chrd = elt[0]
+        dur = elt[1]
+
+        tone = TONES.index(chrd[0])
+        if chrd[1] == 'b':
+            tone -= 1
+        n1 = TONES[tone] + '2'
+        
+        type = chrd[2]
+        tone += 3
+        if type == 'M' or type == '7':
+            tone += 1
+        tone %= 12
+        n2 = TONES[tone] + '2'
+
+        tone += 3
+        if type == 'm':
+            tone += 1
+        tone %= 12
+        n3 = TONES[tone] + '2'
+
+        tone += 3
+        if type == 'M' or type == '0':
+            tone += 1
+        tone %= 12
+        n4 = TONES[tone] + '2'
+
+        prog.append(chord.Chord([n1, n2, n3, n4]))
+        prog[-1].duration.quarterLength = dur
+
+    return prog
 
 def nte (n, dur):
     result = note.Note(n) if (n != "R") else note.Rest()
